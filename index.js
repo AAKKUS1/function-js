a025e38bdac43f9f0b2e3fad12ebecc81151e361
AAKKUS1 <aakkus@edenschool.fr>
2024-11-05 17:14:21 +0100

    feat(app)first-commit

---
+++ index.js
@@ -0,0 +1,88 @@
+//exo1
+//
+//function addition(a, b) {
+ //   return (a + b);
+//}
+//console.log(addition(2, 3)); 
+//
+//exo2
+//function salutation(nom) {
+// return ("Bonjour, " + nom)
+//}
+//console.log(salutation("Alice"));
+//
+//exo3
+//
+//function estPair(nombre) {
+ //   return (nombre % 2  ===0)
+//}
+//console.log(estPair(4)); 
+//console.log(estPair(7));
+//
+//exo4
+//
+//function aireRectangle(longueur, largeur) {
+ //   return (longueur * largeur)
+//}
+//console.log(aireRectangle(5, 3));
+//
+//exo5
+//
+//function estMajuscule(chaine) {
+//return chaine === chaine.toUpperCase();
+//}
+//console.log(estMajuscule("HELLO"));
+//console.log(estMajuscule("Hello"));
+//
+//exo6
+//
+//function maxDeuxNombres(a, b) {
+  //  return Math.max(a, b);
+//}
+//console.log(maxDeuxNombres(5, 10));
+//
+//exo7
+//
+//function difference(a, b) {
+ //   return(a - b)
+//}
+//console.log(difference(10, 3));
+//
+//exo8
+//
+//function convertirEnCelsius(fahrenheit) {
+// return ((fahrenheit - 32) * (5/9))
+//}
+//console.log(convertirEnCelsius(100));
+//
+//exo9
+//
+//function estMajeur(age) {
+ //return age >= 18;
+//}
+//console.log(estMajeur(18));
+//console.log(estMajeur(16));
+//
+//exo10
+// 
+//function maxTroisNombres(a, b, c) {
+//    return Math.max (a, b, c)
+//}
+//console.log(maxTroisNombres(3, 7, 5));
+//
+//exo11
+//
+//function calculer(a, b, operation) {
+ //   return operation(a, b)
+//}
+
+//function addition(x, y) {
+  //return x + y
+//}
+
+//function multiplication(x, y) {
+  //return x * y
+//}
+
+//console.log(calculer(5, 3, addition));
+//console.log(calculer(5, 3, multiplication));
